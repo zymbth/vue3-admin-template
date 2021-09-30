cafbbe19ed2fba40791df1bb0a6612a65546d25a
zymbth <ymzhaobth@gmail.com>
2021-09-30 10:06:35 +0800

    更新vue、elementplus版本

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import 'dayjs/locale/zh-cn'
 import locale from 'element-plus/lib/locale/lang/zh-cn'
 import App from './App.vue'
@@ -29,7 +29,6 @@ const app = createApp(App)
 app.use(ElementPlus, { locale })
 app.use(store)
 app.use(router)
-app.use(window.countTo)
 app.component('svg-icon', SvgIcon)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
