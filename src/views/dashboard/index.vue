01e986571a53ec2ccf52f0482eb8c8b4f3c7ca0f
zymbth <ymzhaobth@gmail.com>
2021-09-30 10:08:16 +0800

    面板增加elementplus calendar，验证国际化

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="container">
-    <div>Black Page: {{$options.name}}</div>
+    <!-- <div>Black Page: {{$options.name}}</div> -->
+    <el-card class="box-card calendar-card">
+      <el-calendar v-model="today">
+        <template #dateCell="{data}" class="weekday">
+          <div :class="data.isSelected ? 'bg-blue' : ''">
+            {{ new Date(data.day).getDate() }} {{ data.isSelected ? '✔️' : '' }}
+          </div>
+        </template>
+      </el-calendar>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Dashboard',
+  data () {
+    return {
+      today: new Date()
+    }
+  }
 }
 </script>
